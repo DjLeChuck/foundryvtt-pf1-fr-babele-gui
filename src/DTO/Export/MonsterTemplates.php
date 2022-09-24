b3d3138f7b447a42b0fa1cf154060627b98f587d
Vivien DE BONA <vdebona@umanit.fr>
2022-09-24 21:49:04 +0200

    feat: rename monster template pack name

--- src/DTO/Export/MonsterTemplates.php
+++ src/DTO/Export/MonsterTemplates.php
@@ -4,7 +4,7 @@ namespace App\DTO\Export;
 
 class MonsterTemplates
 {
-    public string $label = 'Modèles de monstres';
+    public string $label = 'Archétypes de monstres';
     public array $entries;
 
     public function __construct(array $entries)
